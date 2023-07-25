22aa855067962ba5aadb3c4fcc9e58b276600df6
Eastt123 <58124751+Eastt123@users.noreply.github.com>
2023-07-26 03:40:24 +0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,14 +1,11 @@
 let express = require('express');
 let app = express();
 
-const PORT = process.env.PORT;
+
 
 console.log("Hello World");
 
 
-app.listen(PORT,()=>{
-    console.log("listening");
-})
 
 
 
