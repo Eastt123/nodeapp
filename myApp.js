18ab2d0f234f2d1d31211c5157c29de0f4b7218a
Eastt123 <shotabaska@gmail.com>
2023-07-26 03:20:56 +0400

    first commit

---
+++ myApp.js
@@ -0,0 +1,44 @@
+let express = require('express');
+let app = express();
+
+const PORT = process.env.PORT;
+
+console.log("Hello World");
+
+
+app.listen(PORT,()=>{
+    console.log("listening");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
